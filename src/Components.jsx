5d80128e30b1b8c977472e9b003cc87bae936593
Eyasu Araya <128852829+EyasuTes@users.noreply.github.com>
2023-08-12 18:46:38 +0400

    completed initial setup for react using vite.

---
+++ src/Components.jsx
@@ -0,0 +1,18 @@
+//import { useState } from 'react'
+
+import './App.css'
+
+function ComponetOne() {
+
+
+  
+  return (
+     <>
+        
+        <h1>hello</h1>
+        <h2>testing initial setup</h2>
+        <button>Increase age</button>
+     </>
+  )
+} 
+export default ComponetOne;
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ComponentOne from './Components.jsx'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ComponentOne />
+  </React.StrictMode>,
+)
